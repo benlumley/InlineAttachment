cb5b995cfd0cfeda05f1e5b6145e85fb58be2e8e
Ben Lumley <ben@benlumley.co.uk>
2014-03-12 18:24:14 +0000

    Update inline-attach.js
    Allow for in-position placement of image within textareas.

--- src/inline-attach.js
+++ src/inline-attach.js
@@ -138,6 +138,58 @@
                 .replace(/^(\n*)/, "");
         }
 
+        // BL: Have added this method to be used in place of the above, it is based on one I found here:
+        // http://jsfiddle.net/bbE48/13/
+        // I've added the [[B]] and [[A]] bits for removal of extraneous new lines
+        // following same approach as above.
+        // this is then called from this.onReceivedFile;
+        // (for a couple of reasons to do with var scoping, can't do this via the callback args)
+        function insertTextAtTextAreaCursor(el,text) {
+
+            var text = "\n\n[[B]]" + text + "[[A]]\n\n";
+
+
+            var txtarea = el;
+            var scrollPos = txtarea.scrollTop;
+            var strPos = 0;
+            var br = ((txtarea.selectionStart || txtarea.selectionStart == '0') ?
+            "ff" : (document.selection ? "ie" : false ) );
+            if (br == "ie") {
+                txtarea.focus();
+                var range = document.selection.createRange();
+                range.moveStart ('character', -txtarea.value.length);
+                strPos = range.text.length;
+            }
+            else if (br == "ff") strPos = txtarea.selectionStart;
+
+            var front = (txtarea.value).substring(0,strPos);
+            var back = (txtarea.value).substring(strPos,txtarea.value.length);
+            txtarea.value=front+text+back;
+            strPos = strPos + text.length;
+            if (br == "ie") {
+                txtarea.focus();
+                var range = document.selection.createRange();
+                range.moveStart ('character', -txtarea.value.length);
+                range.moveStart ('character', strPos);
+                range.moveEnd ('character', 0);
+                range.select();
+            }
+            else if (br == "ff") {
+                txtarea.selectionStart = strPos;
+                txtarea.selectionEnd = strPos;
+                txtarea.focus();
+            }
+            txtarea.scrollTop = scrollPos;
+
+            var val = txtarea.value;
+            val = val.replace(/(\n{2,})\[\[B\]\]/, "\n\n")
+                .replace(/\[\[A\]\](\n{2,})/, "\n\n")
+                .replace(/^(\n*)/, "");
+            txtarea.value = val;
+
+        }
+
+
         /**
          * When a file has been received by a drop or paste event
          * @param {Blob} file
@@ -146,7 +198,8 @@
             var result = settings.onReceivedFile(file);
             if (result !== false) {
                 lastValue = settings.progressText;
-                editor.setValue(appendInItsOwnLine(editor.getValue(), lastValue));
+                insertTextAtTextAreaCursor(editor.getEl()[0], lastValue);
+                // editor.setValue(appendInItsOwnLine(editor.getValue(), lastValue));
             }
         };
 
